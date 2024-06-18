2b342358d9ffdfb9bf16b4da8e590960b1bf7208
Yan Augusto <yanestricanholi@gmail.com>
2024-06-18 00:45:26 -0300

    feat: Added Header background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,28 +7,32 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
-
 import Logo from "../assets/logo2.png";
 import Bg from "@/assets/headerbg.jpg";
 
 export function Header() {
   return (
     <header>
-      <div className="flex flex-col w-full items-center justify-center h-[316px] gap-4 bg-red-400">
-
-        <img src={Logo} alt="" />
-        <h1 className="font-poppins font-medium text-5xl">Shop</h1>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="font-semibold">
-              <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Shop</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+      <div className="flex flex-col w-full relative items-center justify-center h-[316px] gap-4 bg-red-400">
+        <img
+          src={Bg}
+          className="absolute left-0 top-0 w-full h-full object-cover z-0"
+        />
+        <div className="z-[1] flex flex-col items-center justify-center gap-4">
+          <img src={Logo} alt="" />
+          <h1 className="font-poppins font-medium text-5xl">Shop</h1>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="font-semibold">
+                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Shop</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
       </div>
     </header>
   );
